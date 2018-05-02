1f50e8f6a933e8832dc25e359cec76cd9af57e9e
Jean <jean.gris@easybroadcast.fr>
2018-05-02 13:50:11 +0200

    replace exec by switchs

--- helpers/private/query/run-query.js
+++ helpers/private/query/run-query.js
@@ -36,82 +36,82 @@ module.exports = function runQuery(options, cb) {
     nativeQuery: options.nativeQuery,
     valuesToEscape: options.valuesToEscape,
   })
-  .exec({
+    .switch({
     // If there was an error, check if the connection should be
     // released back into the pool automatically.
-    error: function error(err) {
-      if (!options.disconnectOnError) {
-        return cb(err);
-      }
-
-      releaseConnection(options.connection, options.leased, function releaseConnectionCb(err) {
-        return cb(err);
-      });
-    },
-    // If the query failed, try and parse it into a normalized format and
-    // release the connection if needed.
-    queryFailed: function queryFailed(report) {
-      // Parse the native query error into a normalized format
-      var parsedError;
-      try {
-        parsedError = PG.parseNativeQueryError({
-          nativeQueryError: report.error
-        }).execSync();
-      } catch (e) {
+      error: function error(err) {
         if (!options.disconnectOnError) {
-          return cb(e);
+          return cb(err);
         }
 
-        releaseConnection(options.connection, function releaseConnectionCb() {
-          return cb(e);
+        releaseConnection(options.connection, options.leased, function releaseConnectionCb(err) {
+          return cb(err);
         });
-        return;
-      }
+      },
+      // If the query failed, try and parse it into a normalized format and
+      // release the connection if needed.
+      queryFailed: function queryFailed(report) {
+      // Parse the native query error into a normalized format
+        var parsedError;
+        try {
+          parsedError = PG.parseNativeQueryError({
+            nativeQueryError: report.error
+          }).execSync();
+        } catch (e) {
+          if (!options.disconnectOnError) {
+            return cb(e);
+          }
 
-      // If the catch all error was used, return an error instance instead of
-      // the footprint.
-      var catchAllError = false;
-
-      if (parsedError.footprint.identity === 'catchall') {
-        catchAllError = true;
-      }
-
-      // If this shouldn't disconnect the connection, just return the normalized
-      // error with the footprint.
-      if (!options.disconnectOnError) {
-        if (catchAllError) {
-          return cb(report.error);
+          releaseConnection(options.connection, function releaseConnectionCb() {
+            return cb(e);
+          });
+          return;
         }
 
-        return cb(parsedError);
-      }
+        // If the catch all error was used, return an error instance instead of
+        // the footprint.
+        var catchAllError = false;
 
-      releaseConnection(options.connection, false, function releaseConnectionCb() {
-        if (catchAllError) {
-          return cb(report.error);
+        if (parsedError.footprint.identity === 'catchall') {
+          catchAllError = true;
         }
 
-        return cb(parsedError);
-      });
-    },
-    success: function success(report) {
+        // If this shouldn't disconnect the connection, just return the normalized
+        // error with the footprint.
+        if (!options.disconnectOnError) {
+          if (catchAllError) {
+            return cb(report.error);
+          }
+
+          return cb(parsedError);
+        }
+
+        releaseConnection(options.connection, false, function releaseConnectionCb() {
+          if (catchAllError) {
+            return cb(report.error);
+          }
+
+          return cb(parsedError);
+        });
+      },
+      success: function success(report) {
       //  ╔═╗╔═╗╦═╗╔═╗╔═╗  ┌─┐ ┬ ┬┌─┐┬─┐┬ ┬  ┬─┐┌─┐┌─┐┬ ┬┬ ┌┬┐┌─┐
       //  ╠═╝╠═╣╠╦╝╚═╗║╣   │─┼┐│ │├┤ ├┬┘└┬┘  ├┬┘├┤ └─┐│ ││  │ └─┐
       //  ╩  ╩ ╩╩╚═╚═╝╚═╝  └─┘└└─┘└─┘┴└─ ┴   ┴└─└─┘└─┘└─┘┴─┘┴ └─┘
       // If there was a query type given, parse the results.
-      var queryResults = report.result;
-      if (options.queryType) {
-        try {
-          queryResults = PG.parseNativeQueryResult({
-            queryType: options.queryType,
-            nativeQueryResult: report.result
-          }).execSync();
-        } catch (e) {
-          return cb(e);
+        var queryResults = report.result;
+        if (options.queryType) {
+          try {
+            queryResults = PG.parseNativeQueryResult({
+              queryType: options.queryType,
+              nativeQueryResult: report.result
+            }).execSync();
+          } catch (e) {
+            return cb(e);
+          }
         }
-      }
 
-      return cb(null, queryResults);
-    }
-  });
+        return cb(null, queryResults);
+      }
+    });
 };
